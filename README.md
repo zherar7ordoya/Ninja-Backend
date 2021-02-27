6705e93fba39db6348b12b059c2b6c12e4c60ba3
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-02-27 09:56:37 -0300

    The GraphQLSchema Object

---
+++ README.md
@@ -0,0 +1,29 @@
+# GraphQL Tutorial
+
+In this series we'll be creating, from scratch, a full-stack application, including a GraphQL server on Node.js, a React front-end (with Apollo) and MongoDB to store all of our data.
+
+(YouTube Playlist)[https://www.youtube.com/playlist?list=PL4cUxeGkcC9iK6Qhn-QLcXCXPQUov1U7f]
+
+---
+
+## TIPS
+
+### VSCode Markdown
+
+```json
+{
+  "editor.wordWrap": "wordWrapColumn",
+  "editor.wordWrapColumn": 80,
+  "editor.wordBasedSuggestions": false,
+  "[markdown]": {
+    "editor.wordWrap": "wordWrapColumn",
+    "editor.wordWrapColumn": 80
+  }
+}
+```
+
+### NPM Defaults
+
+- npm config set init.author.email "zherar7ordoya@pm.me"
+- npm config set init.author.name "Gerardo Tordoya"
+- npm config set init.author.url "zherar7ordoya.github.io"
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+const DB_URI =
+  "mongodb+srv://cooluser:coolpassword@cluster0.pjcny.gcp.mongodb.net/ninja?retryWrites=true&w=majority";
+
+/*
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once("open", () => {
+  console.info("MONGODB OPERATIVO");
+});
+*/
+
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    autoIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.info("HUMONGOUS IN DA HOUSE!"))
+  .catch((e) => console.error("*** Error connecting to MongoDB... ***", e));
+
+const app = express();
+
+const { printSchema } = require("graphql");
+
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("***STATUS PUERTO 4000: OKAY***");
+});
+
+// *----------------------------------------------------------------=> NO TOCAR!
+console.log("\n");
+console.log(printSchema(schema));
